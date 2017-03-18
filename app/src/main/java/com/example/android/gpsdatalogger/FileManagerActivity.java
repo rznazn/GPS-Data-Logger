df6553d31fb9916176328ee013eacb9b140d26ee
rznazn <sportrider84@gmail.com>
2017-03-17 19:35:00 -0500

    make file manager activity the opening activity

--- app/src/main/java/com/example/android/gpsdatalogger/FileManagerActivity.java
+++ app/src/main/java/com/example/android/gpsdatalogger/FileManagerActivity.java
@@ -1,14 +1,16 @@
 package com.example.android.gpsdatalogger;
 
-import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by sport on 3/17/2017.
  */
 
-public class FileManagerActivity extends Activity {
+public class FileManagerActivity extends AppCompatActivity {
 
     /**
      * create global variables for views
@@ -19,5 +21,10 @@ public class FileManagerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_file_manager);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_file_directory);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
     }
 }
